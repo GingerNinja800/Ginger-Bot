e7405b60cb6db997be0b715eab907da16c763da3
GingerNinja800 <31391368+GingerNinja800@users.noreply.github.com>
2017-09-26 21:46:54 +0100

    Update Matcher.py

--- Matcher.py
+++ Matcher.py
@@ -221,17 +221,11 @@ async def on_message(message):
            Knight = str(Knight)
            mains = message.content.split(" ")
            mains.remove(mains[0])
-           BadMains = []
-           if len(mains) > 0:
-               for main in mains:
-                   if (main.title() not in AcceptMains) and (main not in "N/A"):
-                       pos = mains.index(main)
-                       BadMains.append(mains[pos])
-              
-               if len(BadMains) > 0:
-                    for BadMain in BadMains:
-                        if BadMain in mains:
-                            mains.remove(mains[BadMain])
+           
+           for pos in range(len(mains)):
+               if (mains[pos].title() not in AcceptMains) and (mains[pos] not in "N/A"):
+                    mains.remove(mains[pos])
+
                         
           
 
