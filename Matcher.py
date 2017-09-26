335f77aae8ba496984ff0597726a4d2e37cece65
GingerNinja800 <31391368+GingerNinja800@users.noreply.github.com>
2017-09-26 19:06:36 +0100

    Update Matcher.py

--- Matcher.py
+++ Matcher.py
@@ -167,7 +167,8 @@ def AddToSheet(name, role, discrim, mains):
                         Row = ColA.index(cell)
                         Row += 2
                         break
-                return "1"
+                print(row)
+                return
 
         except:
             TotalRowCount = (knightsheet.row_count) + 1
