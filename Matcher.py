34e0a5ab62d3decaffa4324dc7c148861841cd68
GingerNinja800 <31391368+GingerNinja800@users.noreply.github.com>
2017-09-27 19:56:55 +0100

    Update Matcher.py

--- Matcher.py
+++ Matcher.py
@@ -8,6 +8,7 @@ import datetime
 
 discordclient = discord.Client()
 
+
 @discordclient.event
 async def on_ready():
     print('Logged in as')
@@ -16,6 +17,7 @@ async def on_ready():
     print("-------")
     await discordclient.change_presence(game=discord.Game(name="Type >>Help"))
 
+
 def AccessSheet():
     scope = ["https://spreadsheets.google.com/feeds"]
     creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
@@ -23,6 +25,7 @@ def AccessSheet():
     knightsheet = client.open("DawnPC Knights").sheet1
     maasheet = client.open("DawnPC Man At Arms").sheet1
     return knightsheet, maasheet
+
 
 def FindSquire(username):
     knightsheet, maasheet = AccessSheet()
@@ -42,12 +45,11 @@ def FindSquire(username):
 
     Canditates = {}
     for Main in KnightMains:
-        for loop in range(1,4):
+        for loop in range(1, 4):
             for Maa in maadata:
                 Maalist = list(Maa.values())
                 if Main == Maalist[loop]:
                     Canditates[Maalist[0]] = Maalist[1:]
-
 
     Matches = {}
     for Canditate in Canditates:
@@ -56,7 +58,7 @@ def FindSquire(username):
 
     Squire = max(Matches, key=Matches.get)
 
-    return(Squire)
+    return (Squire)
 
 
 def FindKnight(username):
@@ -77,13 +79,12 @@ def FindKnight(username):
 
     Canditates = {}
     for Main in maaMains:
-        for loop in range(1,5):
+        for loop in range(1, 5):
             for Knight in knightdata:
                 Knight = list(Knight.values())
                 if Knight[1] == "TRUE":
                     if Main == Knight[loop]:
                         Canditates[Knight[0]] = Knight[2:]
-            
 
     Matches = {}
     for canditate in Canditates:
@@ -91,7 +92,21 @@ def FindKnight(username):
         Matches[canditate] = len(Match)
     KnightTeacher = max(Matches, key=Matches.get)
 
-    return(KnightTeacher)
+    return (KnightTeacher)
+
+
+def FindMe(sheet,discrim):
+    ColA = sheet.col_values(1)
+    ColA.remove(ColA[0])
+    for cell in ColA:
+        cell2 = cell.split("#")
+        if discrim in cell2[1]:
+            Row = ColA.index(cell)
+            Row += 2
+    try:
+        return Row
+    except:
+        return
 
 def AddToSheet(name, role, discrim, mains):
     knightsheet, maasheet = AccessSheet()
@@ -106,11 +121,11 @@ def AddToSheet(name, role, discrim, mains):
                         Row = ColA.index(cell)
                         Row += 2
                         break
-             
+
                 counter = 0
-                maasheet.update_cell(Row,1,name)
-                for cell in range(2,2+len(mains)):
-                    maasheet.update_cell(Row,cell,mains[counter])
+                maasheet.update_cell(Row, 1, name)
+                for cell in range(2, 2 + len(mains)):
+                    maasheet.update_cell(Row, cell, mains[counter])
                     if counter != len(mains):
                         counter += 1
                 return "1"
@@ -131,8 +146,8 @@ def AddToSheet(name, role, discrim, mains):
                 maasheet.insert_row("", index=TotalRowCount)
                 maasheet.update_cell(TotalRowCount, 1, name)
                 counter = 0
-                for cell in range(2,2+len(mains)):
-                    maasheet.update_cell(TotalRowCount,cell,mains[counter])
+                for cell in range(2, 2 + len(mains)):
+                    maasheet.update_cell(TotalRowCount, cell, mains[counter])
                     counter += 1
                 return "1"
             else:
@@ -152,11 +167,11 @@ def AddToSheet(name, role, discrim, mains):
                         Row = ColA.index(cell)
                         Row += 2
                         break
-              
+
                 counter = 0
-                knightsheet.update_cell(Row,1,name)
-                for cell in range(2,2+len(mains)):
-                    knightsheet.update_cell(Row,cell,mains[counter])
+                knightsheet.update_cell(Row, 1, name)
+                for cell in range(3, 3 + len(mains)):
+                    knightsheet.update_cell(Row, cell, mains[counter])
                     if counter != len(mains):
                         counter += 1
                 return "1"
@@ -169,7 +184,7 @@ def AddToSheet(name, role, discrim, mains):
                         Row = ColA.index(cell)
                         Row += 2
                         break
-                print(row)
+                print(Row)
                 return
 
         except:
@@ -178,8 +193,8 @@ def AddToSheet(name, role, discrim, mains):
                 knightsheet.insert_row("", index=TotalRowCount)
                 knightsheet.update_cell(TotalRowCount, 1, name)
                 counter = 0
-                for cell in range(2,2+len(mains)):
-                    knightsheet.update_cell(TotalRowCount,cell,mains[counter])
+                for cell in range(3, 3 + len(mains)):
+                    knightsheet.update_cell(TotalRowCount, cell, mains[counter])
                     if counter != len(mains):
                         counter += 1
                 return "1"
@@ -191,16 +206,51 @@ def AddToSheet(name, role, discrim, mains):
     else:
         return
 
+def TransferData(name,discrim):
+    knightsheet, maasheet = AccessSheet()
+    Row = FindMe(maasheet,discrim)
+    if Row == None:
+        return
+    else:
+        RowValues = maasheet.row_values(Row)
+        RowValues.insert(1,"")
+        maasheet.delete_row(Row)
+
+        present = FindMe(knightsheet,discrim)
+        print(present)
+        if present == None:
+            TotalRowCount = (knightsheet.row_count)+1
+            knightsheet.insert_row(RowValues,index = TotalRowCount)
+            return "1"
+        else:
+            for cell in range(3,3+len(RowValues)):
+                knightsheet.update_cell(present,cell,RowValues[cell-1])
+            return "1"
+
+
+def Squiring(Finding,squiring,discrim):
+        knightsheet, maasheet = AccessSheet()
+        Row = FindMe(knightsheet,discrim)
+        if Finding == "?":
+            if Row == None:
+                return
+            else:
+                return (knightsheet.row_values(Row)[1])
+        elif Finding == "!":
+            knightsheet.update_cell(Row,2,squiring)
+            return
+
 @discordclient.event
 async def on_message(message):
     if message.author.name != "Ginger Bot":
         username = message.author.name
+        discrim = message.author.discriminator
         if message.content.startswith("!FindKnight"):
             try:
                 Knight = FindKnight(username)
                 await discordclient.send_message(message.channel, Knight)
             except:
-                await discordclient.send_message(message.channel,"Sorry there was an error")
+                await discordclient.send_message(message.channel, "Sorry there was an error")
 
         elif message.content.startswith("!FindSquire"):
             try:
@@ -209,62 +259,77 @@ async def on_message(message):
             except:
                 await discordclient.send_message(message.channel, "Sorry there was an error")
         elif message.content.startswith(">>Help"):
-            await discordclient.send_message(message.channel, "!FindSquire : Suggests the most worthy MaA for your knightliness\n!FindKnight : Suggests the most suitable knight for your squireship\n!AddMe : Adds you to the relevant spreadsheet")
+            await discordclient.send_message(message.channel,
+                                             "!FindSquire : Suggests the most worthy MaA for your knightliness\n!FindKnight : Suggests the most suitable knight for your squireship\n!AddMe : Adds you to the relevant spreadsheet")
 
         elif message.content.startswith("!AddMe"):
-           username += "#" + message.author.discriminator
-           discrim = message.author.discriminator
-           AcceptMains = ["True","False","Warden","Conqueror","Peacekeeper","Lawbringer","Centurion","Gladiator","Raider","Warlord","Berzerker","Valkyrie","Highlander","Kensei","Shugoki","Orochi","Nobushi","Shinobi"]
-           ManAtArms = discord.utils.get(message.author.roles, name="Man At Arms")
-           ManAtArms = str(ManAtArms)
-           Knight = discord.utils.get(message.author.roles, name="Knight")
-           Knight = str(Knight)
-           mains = message.content.split(" ")
-           mains.remove(mains[0])
-           validated = []
-           for pos in range(len(mains)):
-               if (mains[pos].title() in AcceptMains) or (mains[pos] in "N/A"):
-                    if mains[pos] in AcceptMains[:1]:
-                        if validated[0] not in AcceptMains[:1]:
-                            validated.append(mains[pos])
-                        
-                    elif mains[pos] in AcceptMains[2:]:
-                        if len(validated) >= 1:
-                            validated.append(mains[pos])
-                        else:
-                            validated.append("")
-                            validated.append(mains[pos])
-                    else:
-                        validated.append("")
-                        
-                         
-           mains = validated
-           
-           
+            username += "#" + message.author.discriminator
+            AcceptMains = ["True", "False", "Warden", "Conqueror", "Peacekeeper", "Lawbringer", "Centurion",
+                           "Gladiator", "Raider", "Warlord", "Berzerker", "Valkyrie", "Highlander", "Kensei", "Shugoki",
+                           "Orochi", "Nobushi", "Shinobi"]
+            ManAtArms = discord.utils.get(message.author.roles, name="Man At Arms")
+            ManAtArms = str(ManAtArms)
+            Knight = discord.utils.get(message.author.roles, name="Knight")
+            Knight = str(Knight)
+            mains = message.content.split(" ")
+            mains.remove(mains[0])
+            validated = []
+            for pos in range(len(mains)):
+                if (mains[pos].title() in AcceptMains):
+                    if mains[pos] in AcceptMains[2:]:
+                        validated.append(mains[pos])
 
-           if ManAtArms != "None":
-               none = AddToSheet(username, ManAtArms, discrim,mains)
-           elif Knight != "None":
-               none = AddToSheet(username, Knight, discrim,mains)
-           if none == None:
-               await discordclient.send_message(message.channel, "You're already in the spreadsheet.")
-           else:
-               await discordclient.send_message(message.channel, "Success!")
-        
+            mains = validated
+
+            if ManAtArms != "None":
+                none = AddToSheet(username, ManAtArms, discrim, mains)
+            elif Knight != "None":
+                none = AddToSheet(username, Knight, discrim, mains)
+            if none == None:
+                await discordclient.send_message(message.channel, "You're already in the spreadsheet.")
+            else:
+                await discordclient.send_message(message.channel, "Success!")
+
         elif message.content.startswith("!PromoteMe"):
-           joindate = str(message.author.joined_at).split(" ")[0]
-           todaydate = datetime.date.today()
-           JoinDate2 = joindate
-           joindate = joindate.split("-")
-           
-           joindate = datetime.date(int(joindate[0]),int(joindate[1]),int(joindate[2]))
-           print(joindate)
-           difference = str(todaydate - joindate).split()[0]
-           if int(difference) >= 7:
-               await discordclient.add_roles(message.author, discord.utils.get(message.server.roles, name="Man At Arms"))
-               await discordclient.remove_roles(message.author, discord.utils.get(message.server.roles, name="Recruit"))
-               await discordclient.send_message(message.channel,"Congratulations, you're now a Man At Arms. Do !AddMe to add yourself to the database to aid in squiring")
-           else:
-               await discordclient.send_message(message.channel,"You joined "+JoinDate2+". You must wait "+difference+" days before you can become a Man At Arms. #SorryNotSorry")
+            if str(discord.utils.get(message.author.roles, name = "Recruit")) == "Recruit":
+                joindate = str(message.author.joined_at).split(" ")[0]
+                todaydate = datetime.date.today()
+                JoinDate2 = joindate
+                joindate = joindate.split("-")
+
+                joindate = datetime.date(int(joindate[0]), int(joindate[1]), int(joindate[2]))
+                print(joindate)
+                difference = str(todaydate - joindate).split()[0]
+                if int(difference) >= 7:
+                    await discordclient.add_roles(message.author,
+                                                  discord.utils.get(message.server.roles, name="Man At Arms"))
+                    await discordclient.remove_roles(message.author,
+                                                     discord.utils.get(message.server.roles, name="Recruit"))
+                    await discordclient.send_message(message.channel,
+                                                     "Congratulations, you're now a Man At Arms. Do !AddMe to add yourself to the database to aid in squiring")
+                else:
+                    await discordclient.send_message(message.channel,
+                                                     "You joined " + JoinDate2 + ". You must wait " + difference + " days before you can become a Man At Arms. #SorryNotSorry")
+
+            elif str(discord.utils.get(message.author.roles, name = "Knight")) == "Knight":
+                Done = TransferData(username,discrim)
+                if Done == "1":
+                    await discordclient.send_message(message.channel, "You've been transferred to the Knight sheet. Remember to add whether or not you're available to squire")
+                elif Done == None:
+                    await discordclient.send_message(message.channel, "Error: You're most likely not in the MaA sheet, so just use !AddMe to add yourself to knight sheet if not already there.")
+
+        elif message.content.startswith("!Squiring") or message.content.startswith("?Squiring"):
+            if str(discord.utils.get(message.author.roles, name = "Knight")) == "Knight":
+                userinput = message.content.split(" ")
+                finding = userinput[0][0]
+                if finding == "!":
+                    userinput.remove(userinput[0])
+                    if userinput[0].title() in ["True","False","N/A"]:
+                        squiring = userinput[0].title()
+                        Squiring(finding,squiring,discrim)
+                        await discordclient.send_message(message.channel, "Done.")
+                elif finding == "?":
+                    squireless = Squiring(finding,"",discrim)
+                    await discordclient.send_message(message.channel, "You're current status is: "+squireless)
 
 discordclient.run('MzM2MTI4OTc3MzA1NDY4OTI4.DIR5cA.SVdKgvWIgkqw2zzTtyrL9RBAB54')
