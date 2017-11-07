65a5d9cad37e2ac0481c7ef2e2028d88da08f656
GingerNinja800 <31391368+GingerNinja800@users.noreply.github.com>
2017-11-07 18:56:28 +0000

    Update Matcher.py

--- Matcher.py
+++ Matcher.py
@@ -374,7 +374,7 @@ async def on_message(message):
             creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
             client = gspread.authorize(creds)
             DawnPCSheet = client.open("The Holy Sepulchre Members").sheet1
-            if str(discord.utils.get(message.author.roles,name = "Dawn Knight Commander")) != "Dawn Knight Commander" or str(discord.utils.get(message.author.roles,name = "Templar Commander")) != "Templar Commander" or discrim != "2241":
+            if str(discord.utils.get(message.author.roles,name = "Dawn Knight Commander")) != "Dawn Knight Commander" or str(discord.utils.get(message.author.roles,name = "Templar Commander")) != "Templar Commander" or discrim != 2241:
                 await discordclient.send_message(message.channel, "Leadership only for this command, sorry lads")
             else:
                 for person in discordclient.get_all_members():
